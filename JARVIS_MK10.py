22a7e1ebe332893e488b859b4e6489a7b6519cd5
ARYAN SAINI <saini.aryan9999@gmail.com>
2023-06-24 22:45:20 +0530

    save

--- JARVIS_MK10.py
+++ JARVIS_MK10.py
@@ -77,7 +77,7 @@ closing1=nlp("thank you, goodbye jarvis")
 closing2=nlp("bye")
 closing3=nlp("close jarvis")
 closing4=nlp("goodbye jarvis")
-closing5=nlp("that's all for now, goodbye jarvis")
+closing5=nlp("that's all for now goodbye jarvis")
 stopListening1=nlp("jarvis stop listening")
 stopListening2=nlp("jarvis stop listening for a bit")
 startListening1=nlp("jarvis start listening")
@@ -85,7 +85,7 @@ startListening2=nlp("okay jarvis wake up")
 openApps=nlp("jarvis, please open")
 searchFor=nlp("search for")
 playOnYT=nlp("play on youtube")
-setVolume=nlp("jarvis, set volume to some percent")
+setVolume=nlp("jarvis, set volume to this percent")
 play=nlp("jarvis play the music")
 play1=nlp("play song")
 pause=nlp("jarvis pause the music")
@@ -111,6 +111,8 @@ time=nlp("what time is it?")
 date=nlp("what date is it?")
 time1=nlp("tell me the time")
 date1=nlp("tell me the date")
+increaseVolume=nlp("volume increased by ")
+decreaseVolume=nlp("volume decreased by ")
 
 # choose input as text or voice
 choice=input("Enter your mode of input 't' for text and ('v' or press enter) for voice: ")
@@ -139,7 +141,7 @@ while True:
             continue
     
     # deciding output based on different inputs i.e text
-    if (((nlp(text.lower())).similarity(closing1)>=0.7) or ((nlp(text.lower())).similarity(closing2)>=0.7)or ((nlp(text.lower())).similarity(closing4)>=0.77) or ((nlp(text.lower())).similarity(closing3)>=0.7) or ((nlp(text.lower())).similarity(closing5)>=0.7)) and ((nlp(text.lower())).similarity(startListening1)<0.75 and ((nlp(text.lower()).similarity(closing3))>nlp(text.lower()).similarity(closeWindow))):
+    if (((nlp(text.lower())).similarity(closing1)>=0.7) or ((nlp(text.lower())).similarity(closing2)>=0.7) or ((nlp(text.lower())).similarity(closing4)>=0.7) or ((nlp(text.lower())).similarity(closing3)>=0.7) or ((nlp(text.lower())).similarity(closing5)>=0.7)) and (nlp(text.lower())).similarity(startListening1)<0.7 and (0.7>nlp(text.lower()).similarity(closeWindow)) and ("close" in text.lower() or "goodbye" in text.lower() or "bye" in text.lower()):
         response="i hope i was helpful, Until next time sir."
         responseProtocol(response)
         writeInMemory(text,response)
@@ -153,7 +155,7 @@ while True:
                     f.write(lines[i+1]+"\n")
                 i+=1
         break
-    elif(nlp(text.lower()).similarity(time)>0.7 or nlp(text.lower()).similarity(date)>0.7 or nlp(text.lower()).similarity(time1)>0.7 or nlp(text.lower()).similarity(date1)>0.7):
+    elif(nlp(text.lower()).similarity(time)>0.7 or nlp(text.lower()).similarity(date)>0.7 or nlp(text.lower()).similarity(time1)>0.7 or nlp(text.lower()).similarity(date1)>0.7) and ("time" in text.lower() or "date" in text.lower()):
         if "time" in text.lower():
             current_time=datetime.datetime.now().strftime("%I:%M %p")
             response="It's "+current_time
@@ -162,9 +164,9 @@ while True:
         elif "date" in text.lower():
             current_date=datetime.date.today()
             response="Today is "+str(current_date)
-            responseProtocol()
-            writeInMemory()
-    elif ((nlp(text.lower())).similarity(stopListening1)>=0.75 or (nlp(text.lower())).similarity(stopListening2)>=0.75) and ((nlp(text.lower())).similarity(stopListening1)>(nlp(text.lower())).similarity(startListening1)):
+            responseProtocol(response)
+            writeInMemory(text,response)
+    elif ((nlp(text.lower())).similarity(stopListening1)>=0.75 or (nlp(text.lower())).similarity(stopListening2)>=0.75) and ((nlp(text.lower())).similarity(stopListening1)>(nlp(text.lower())).similarity(startListening1)) and ("stop listening" in text.lower()):
         response="Going to sleep mode"
         responseProtocol(response)
         writeInMemory(text,response)
@@ -186,7 +188,7 @@ while True:
                 except sr.RequestError as e:
                     print("Could not request results from Google Speech Recognition service; {0}".format(e))
                     continue
-            if ((nlp(text.lower())).similarity(startListening1)>=0.75 or (nlp(text.lower())).similarity(startListening2)>=0.75) and ((nlp(text.lower())).similarity(startListening1)>(nlp(text.lower())).similarity(stopListening1)):
+            if ((nlp(text.lower())).similarity(startListening1)>=0.75 or (nlp(text.lower())).similarity(startListening2)>=0.75) and ((nlp(text.lower())).similarity(startListening1)>(nlp(text.lower())).similarity(stopListening1)) and ("start listening" in text.lower() or "wake up" in text.lower()):
                 print("You said: ", text)
                 response="Coming back online"
                 responseProtocol(response)
@@ -194,7 +196,7 @@ while True:
                 break
             else:
                 continue
-    elif (nlp(text.lower())).similarity(openApps)>=0.75 and ((nlp(text.lower())).similarity(switchWindow)<(nlp(text.lower())).similarity(openApps)):
+    elif (nlp(text.lower())).similarity(openApps)>=0.75 and ((nlp(text.lower())).similarity(switchWindow)<(nlp(text.lower())).similarity(openApps)) and ("open" in text.lower()):
         app=""
         words=text.split()
         index=words.index("open")
@@ -234,16 +236,15 @@ while True:
             redirect_uri="http://localhost:8080",
             scope="user-read-playback-state,user-modify-playback-state"))
             sp.next_track()
-    elif (((nlp(text.lower())).similarity(play)>=0.7) or ((nlp(text.lower())).similarity(pause)>=0.7) or ((nlp(text.lower())).similarity(pause1)>=0.7) or ((nlp(text.lower())).similarity(play1)>=0.7) or ((nlp(text.lower())).similarity(pause2)>=0.7)) and (((nlp(text.lower())).similarity(mute)<(nlp(text.lower())).similarity(play)) or ((nlp(text.lower())).similarity(unmute)<(nlp(text.lower())).similarity(pause))):
+    elif (((nlp(text.lower())).similarity(play)>=0.7) or ((nlp(text.lower())).similarity(pause)>=0.7) or ((nlp(text.lower())).similarity(pause1)>=0.7) or ((nlp(text.lower())).similarity(play1)>=0.7) or ((nlp(text.lower())).similarity(pause2)>=0.7)) and (((nlp(text.lower())).similarity(mute)<(nlp(text.lower())).similarity(play)) or ((nlp(text.lower())).similarity(unmute)<(nlp(text.lower())).similarity(pause))) and ("play" in text.lower() or "pause" in text.lower()):
         pyautogui.press("playpause")
-    elif (nlp(text.lower())).similarity(setVolume)>=0.6 and ((nlp(text.lower())).similarity(switchWindow)<(nlp(text.lower())).similarity(setVolume)):
+    elif (nlp(text.lower())).similarity(setVolume)>=0.7 and ((nlp(text.lower())).similarity(switchWindow)<(nlp(text.lower())).similarity(setVolume)) and ("volume" in text.lower()):
         words=text.split()
         if words[-1].lower()=="percent" or words[-1].lower()=="percentage" or words[-1].lower()=="%":
             words=words[:-1]
         if words[-1][-1].lower()=="%":
             words[-1]=words[-1][:-1]
-        if text.lower()!="mute" and text.lower()!="unmute":
-            newLevel=int(words[-1])
+        newLevel=int(words[-1])
         devices = AudioUtilities.GetSpeakers()
         interface = devices.Activate(
             IAudioEndpointVolume._iid_, 
@@ -259,7 +260,7 @@ while True:
         else:
             factor=int((-1)*((level-newLevel)/2))                
             pyautogui.press('volumeup', presses=factor)
-    elif (nlp(text.lower())).similarity(mute)>=0.75 or (nlp(text.lower())).similarity(unmute)>=0.75 or (nlp(text.lower())).similarity(mute1)>=0.75 or (nlp(text.lower())).similarity(unmute1)>=0.75 or (nlp(text.lower())).similarity(mute2)>=0.75 or (nlp(text.lower())).similarity(unmute2)>=0.75:
+    elif ((nlp(text.lower())).similarity(mute)>=0.75 or (nlp(text.lower())).similarity(unmute)>=0.75 or (nlp(text.lower())).similarity(mute1)>=0.75 or (nlp(text.lower())).similarity(unmute1)>=0.75 or (nlp(text.lower())).similarity(mute2)>=0.75 or (nlp(text.lower())).similarity(unmute2)>=0.75) and ("mute" in text.lower() or "unmute" in text.lower()):
         devices = AudioUtilities.GetSpeakers()
         interface = devices.Activate(
             IAudioEndpointVolume._iid_, 
@@ -271,24 +272,24 @@ while True:
             volume.SetMute(0, None)
         elif "mute" in text.lower():
             volume.SetMute(1,None)
-    elif (nlp(text.lower())).similarity(scroll)>=0.75 and (nlp(text.lower())).similarity(closeWindow)<(nlp(text.lower())).similarity(scroll):
+    elif (nlp(text.lower())).similarity(scroll)>=0.75 and (nlp(text.lower())).similarity(closeWindow)<(nlp(text.lower())).similarity(scroll) and ("scroll" in text.lower()):
             i=800
             sensi=i
             if "down" in text.lower():
                 sensi=-i
             pyautogui.scroll(sensi)
-    elif (nlp(text.lower())).similarity(closeWindow)>0.75 and ((nlp(text.lower())).similarity(closeWindow)>(nlp(text.lower())).similarity(switchWindow)):
+    elif (nlp(text.lower())).similarity(closeWindow)>0.75 and ((nlp(text.lower())).similarity(closeWindow)>(nlp(text.lower())).similarity(switchWindow)) and ("close" in text.lower()):
         window = win32gui.GetForegroundWindow()
         win32gui.PostMessage(window, win32con.WM_CLOSE, 0, 0)
-    elif (nlp(text.lower())).similarity(switchWindow)>0.75 and (nlp(text.lower())).similarity(switchtab)<(nlp(text.lower())).similarity(switchWindow):
+    elif (nlp(text.lower())).similarity(switchWindow)>0.75 and (nlp(text.lower())).similarity(switchtab)<(nlp(text.lower())).similarity(switchWindow) and ("switch" in text.lower() or "change" in text.lower()):
         pyautogui.hotkey('alt', 'tab')
-    elif (nlp(text.lower())).similarity(switchtab)>0.75 and (nlp(text.lower())).similarity(switchtab)>(nlp(text.lower())).similarity(switchWindow) and (nlp(text.lower())).similarity(switchtab)>(nlp(text.lower())).similarity(showAllTabs):
+    elif (nlp(text.lower())).similarity(switchtab)>0.75 and (nlp(text.lower())).similarity(switchtab)>(nlp(text.lower())).similarity(switchWindow) and (nlp(text.lower())).similarity(switchtab)>(nlp(text.lower())).similarity(showAllTabs) and ("switch" in text.lower or "change" in text.lower()):
         pyautogui.hotkey('ctrl', 'tab')
-    elif (nlp(text.lower())).similarity(showAllTabs)>0.75:
+    elif (nlp(text.lower())).similarity(showAllTabs)>0.75 and ("show" in text.lower()):
         pyautogui.hotkey("win","tab")
-    elif (nlp(text.lower())).similarity(lock)>0.75:
+    elif (nlp(text.lower())).similarity(lock)>0.75 and ("lock" in text.lower()):
         os.system("rundll32.exe user32.dll, LockWorkStation")
-    elif (nlp(text.lower())).similarity(ytFullscreen)>0.8 or (nlp(text.lower())).similarity(ytFullscreen1)>0.8 or (nlp(text.lower())).similarity(exitYTFullscreen)>0.75 or (nlp(text.lower())).similarity(ytFullscreen2)>0.75:
+    elif (nlp(text.lower())).similarity(ytFullscreen)>0.8 or (nlp(text.lower())).similarity(ytFullscreen1)>0.8 or (nlp(text.lower())).similarity(exitYTFullscreen)>0.75 or (nlp(text.lower())).similarity(ytFullscreen2)>0.75 and ("fullscreen" in text.lower() or "full screen" in text.lower()):
         pyautogui.hotkey('f')
     elif text.lower().strip().startswith("remember that") or text.lower().strip().startswith("remember this") or text.lower().strip().endswith("remember that") or text.lower().strip().endswith("remember this") or  text.lower().strip().startswith("jarvis remember that") or text.lower().strip().startswith("jarvis remember this"):
         response="okay i'll remember that."
@@ -314,14 +315,14 @@ while True:
         response=" ".join(words)
         
         #deciding output based on different responses by the model i.e. response
-        if "volume decreased by " in response.lower() or "volume increased by " in response.lower() or "increasing volume by " in response.lower() or "decreasing volume by " in response.lower() or "volume has been decreased by " in response.lower() or "volume has been increased by " in response.lower():
+        if (nlp(response.lower()).similarity(increaseVolume) or nlp(response.lower()).similarity(decreaseVolume)) and ("volume" in response.lower()):
+            i=0
             words=text.split()
             if words[-1]=="percent" or words[-1]=="percentage":
                 words=words[:-1]
             if words[-1][-1]=="%":
                 words[-1]=words[-1].replace("%",'')
             factor=int(words[-1])
-            i=0
             for i in range(int(factor/2)):
                 if "decreased" in response:
                     keyboard.press_and_release('volume down')
@@ -348,28 +349,35 @@ while True:
             if appName.lower()=="flipkart" or appName.lower()=="google" or appName.lower()=="youtube":
                 url = "https://www."+appName.lower()+".com/search?q=" + searchQuery
                 response="Searching for "+searchQuery+" on "+appName
+                responseProtocol(response)
                 writeInMemory(text,response)
             elif appName.lower()=="amazon":
                 url = "https://www.amazon.in/s?k=" +searchQuery
                 response="Searching for "+searchQuery+" on "+appName
+                responseProtocol(response)
                 writeInMemory(text,response)
             elif appName.lower()=="myntra":
                 url = "https://www.myntra.com/"+searchQuery+"?rawQuery="+searchQuery
                 response="Searching for "+searchQuery+" on "+appName
+                responseProtocol(response)
                 writeInMemory(text,response)
             elif appName.lower()=="ajio":
                 url = "https://www.ajio.com/search/?text="+searchQuery
                 response="Searching for "+searchQuery+" on "+appName
+                responseProtocol(response)
                 writeInMemory(text,response)
             elif appName.lower()=="nykaa":
                 url = "https://www.nykaa.com/search/result/?q="+searchQuery+"&root=search&searchType=Manual&sourcepage=Search+Page"
                 response="Searching for "+searchQuery+" on "+appName
+                responseProtocol(response)
                 writeInMemory(text,response)
             elif appName.lower()=="primevideo":
                 url = "https://www.primevideo.com/search/ref=atv_nb_sug?ie=UTF8&phrase="+searchQuery
                 response="Searching for "+searchQuery+" on "+appName
+                responseProtocol(response)
                 writeInMemory(text,response)
             webbrowser.open_new_tab(url) 
+            continue
         elif "playing" in response.lower() and "on youtube" in response.lower():
             start = text.lower().find("play")
             end = text.lower().find("on youtube")
@@ -385,6 +393,7 @@ while True:
                 responseProtocol(response)
                 writeInMemory(text,response)
                 webbrowser.open(url)
+                continue
             except HttpError as e:
                 print('An error occurred:', e)
                 url = None
@@ -394,7 +403,9 @@ while True:
                     if 'youtube.com' in j:
                         webbrowser.open(j)
                 response="There are plenty of good videos, i think you should choose which one to watch."
+                responseProtocol(response)
                 writeInMemory(text,response)
+                continue
                 
         responseProtocol(response)
         with open('Conversation.txt', 'a') as file:
--- practice.py
+++ practice.py
@@ -2,15 +2,23 @@ import spacy
 
 nlp = spacy.load("en_core_web_lg")
 
-sentence2 = "jarvis what time is it?"
-sentence1 = "what is the time?"
-# sentence3="jarvis, please open youtube"
+# sentence2 = "goodbye jarvis "
+# sentence1 = "okay goodbye jarvis"
+# # sentence3="jarvis, please open youtube"
 
-doc1 = nlp(sentence1)
-doc2 = nlp(sentence2)
-# doc3=nlp(sentence3)
+# doc1 = nlp(sentence1)
+# doc2 = nlp(sentence2)
+# # doc3=nlp(sentence3)
 
-# print(doc2.similarity(doc3))
-# print(doc1.similarity(doc3))
-print(doc1.similarity(doc2))
+# # print(doc2.similarity(doc3))
+# # print(doc1.similarity(doc3))
+# print(doc1.similarity(doc2))
 
+closing1=nlp("thank you, goodbye jarvis")
+closing2=nlp("bye")
+closing3=nlp("close jarvis")
+closing4=nlp("goodbye jarvis")
+closing5=nlp("that's all for now goodbye jarvis")
+text="that's all for now jarvis goodbye"
+if (((nlp(text.lower())).similarity(closing1)>=0.7) or ((nlp(text.lower())).similarity(closing2)>=0.7) or ((nlp(text.lower())).similarity(closing4)>=0.7) or ((nlp(text.lower())).similarity(closing3)>=0.7) or ((nlp(text.lower())).similarity(closing5)>=0.7)):
+    print("hello")
